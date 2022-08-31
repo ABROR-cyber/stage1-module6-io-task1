a251a8a8194354ee9d53294d3b7af3e401afb723
abror <abror01042001@gmail.com>
2022-09-01 01:47:40 +0500

    com

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 public class FileReader {
 
+
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
         try (FileInputStream reader = new FileInputStream(file)) {
@@ -18,23 +19,29 @@ public class FileReader {
                 else
                     stringBuilder.append((char) c);
             }
+            stringBuilder.insert(stringBuilder.indexOf("Age")-1,"end");
+            stringBuilder.insert(stringBuilder.indexOf("Email")-1,"end");
+            stringBuilder.insert(stringBuilder.indexOf("Phone")-1,"end");
+            stringBuilder.insert(stringBuilder.length()-1,"end");
+
             String s = stringBuilder.toString();
             for (int i = 0; i < 4; i++) {
-                String text = s.substring(s.indexOf("start") + 5, s.indexOf("\r"));
+                String text = s.substring(s.indexOf("start") + 5, s.indexOf("end")-1);
                 if (i==0)
                     profile.setName(text);
                 else if (i==1)
                     profile.setAge(Integer.parseInt(text));
                 else if (i==2)
                     profile.setEmail(text);
-                else profile.setPhone(Long.parseLong(text));
+                else if (i==3)
+                    profile.setPhone(Long.parseLong(text));
                 if (i != 3)
-                        s = s.substring(s.indexOf("\r") + 2);
+                        s = s.substring(s.indexOf("end") + 3);
 
             }
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
         return profile;
