0df79d896aaf7449929cf5b4414adef9ab5118c4
abror <abror01042001@gmail.com>
2022-09-01 00:33:41 +0500

    ccc

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,27 +7,31 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-        try (FileInputStream reader = new FileInputStream(file)){
+        try (FileInputStream reader = new FileInputStream(file)) {
 
             byte[] bytes = new byte[reader.available()];
             reader.read(bytes);
             StringBuilder stringBuilder = new StringBuilder();
             for (byte c : bytes) {
-                stringBuilder.append((char) c);
+                if (c == ' ')
+                    stringBuilder.append("start");
+                else
+                    stringBuilder.append((char) c);
             }
             String s = stringBuilder.toString();
             System.out.println(s);
             for (int i = 0; i < 4; i++) {
-                String temp = s.substring(s.indexOf(" ") + 1, s.indexOf("\r"));
-                s = s.substring(s.indexOf("\n") + 1);
+                String text = s.substring(s.indexOf("start") + 5, s.indexOf("\r"));
                 if (i==0)
-                    profile.setName(temp);
+                    profile.setName(text);
                 else if (i==1)
-                    profile.setAge(Integer.parseInt(temp));
+                    profile.setAge(Integer.parseInt(text));
                 else if (i==2)
-                    profile.setEmail(temp);
+                    profile.setEmail(text);
                 else if (i==3)
-                    profile.setPhone(Long.parseLong(temp));
+                    profile.setPhone(Long.parseLong(text));
+                if (i != 3)
+                        s = s.substring(s.indexOf("\r") + 2);
 
             }
 
