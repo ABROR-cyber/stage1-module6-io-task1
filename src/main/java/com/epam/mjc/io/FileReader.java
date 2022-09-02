b1e332fd5457546e53751bbca8f18139aa637def
abror <abror01042001@gmail.com>
2022-09-02 17:14:48 +0500

    commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -12,21 +12,22 @@ public class FileReader {
         Profile profile = new Profile();
         try (FileInputStream reader = new FileInputStream(file)) {
             byte[] bytes = new byte[reader.available()];
-            reader.read(bytes);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (byte c : bytes) {
-                stringBuilder.append((char) c);
+            while (reader.read(bytes)>0) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (byte c : bytes) {
+                    stringBuilder.append((char) c);
+                }
+                String s = stringBuilder.toString();
+                s = s.replace("\r\n", end);
+                s = s.replace(": ", start);
+                profile.setName(s.substring(s.indexOf(start) + 9, s.indexOf(end)));
+                s = s.substring(s.indexOf(end) + 3);
+                profile.setAge(Integer.parseInt(s.substring(s.indexOf(start) + 9, s.indexOf(end))));
+                s = s.substring(s.indexOf(end) + 7);
+                profile.setEmail(s.substring(s.indexOf(start) + 9, s.indexOf(end)));
+                s = s.substring(s.indexOf(end) + 7);
+                profile.setPhone(Long.parseLong(s.substring(s.indexOf(start) + 9, s.indexOf(end))));
             }
-            String s = stringBuilder.toString();
-            s=s.replace("\r\n",end);
-            s=s.replace(": ",start);
-            profile.setName(s.substring(s.indexOf(start)+5,s.indexOf(end)));
-            s=s.substring(s.indexOf(end)+3);
-            profile.setAge(Integer.parseInt(s.substring(s.indexOf(start)+5,s.indexOf(end))));
-            s=s.substring(s.indexOf(end)+3);
-            profile.setEmail(s.substring(s.indexOf(start)+5,s.indexOf(end)));
-            s=s.substring(s.indexOf(end)+3);
-            profile.setPhone(Long.parseLong(s.substring(s.indexOf(start)+5,s.indexOf(end))));
         } catch (IOException e) {
             e.printStackTrace();
         }
