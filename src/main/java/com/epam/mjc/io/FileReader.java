66b83a4260e0249c9e18a8a19f46d8fa874fd16f
abror <abror01042001@gmail.com>
2022-09-02 17:06:12 +0500

    commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 
 public class FileReader {
+    final String start="startWord";
+    final String end="endWord";
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
         try (FileInputStream reader = new FileInputStream(file)) {
@@ -16,15 +18,15 @@ public class FileReader {
                 stringBuilder.append((char) c);
             }
             String s = stringBuilder.toString();
-            s=s.replace("\r\n","end");
-            s=s.replace(": ","start");
-            profile.setName(s.substring(s.indexOf("start")+5,s.indexOf("end")));
-            s=s.substring(s.indexOf("end")+3);
-            profile.setAge(Integer.parseInt(s.substring(s.indexOf("start")+5,s.indexOf("end"))));
-            s=s.substring(s.indexOf("end")+3);
-            profile.setEmail(s.substring(s.indexOf("start")+5,s.indexOf("end")));
-            s=s.substring(s.indexOf("end")+3);
-            profile.setPhone(Long.parseLong(s.substring(s.indexOf("start")+5,s.indexOf("end"))));
+            s=s.replace("\r\n",end);
+            s=s.replace(": ",start);
+            profile.setName(s.substring(s.indexOf(start)+5,s.indexOf(end)));
+            s=s.substring(s.indexOf(end)+3);
+            profile.setAge(Integer.parseInt(s.substring(s.indexOf(start)+5,s.indexOf(end))));
+            s=s.substring(s.indexOf(end)+3);
+            profile.setEmail(s.substring(s.indexOf(start)+5,s.indexOf(end)));
+            s=s.substring(s.indexOf(end)+3);
+            profile.setPhone(Long.parseLong(s.substring(s.indexOf(start)+5,s.indexOf(end))));
         } catch (IOException e) {
             e.printStackTrace();
         }
